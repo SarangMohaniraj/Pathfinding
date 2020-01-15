8df655a8fc51c642aa3bacfe479deca798f0c467
Sarang Mohaniraj <10013063@sbschools.org>
2020-01-15 14:17:07 -0500

    Create grid in Unity3d

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public float nodeRadius;
+    private Node[,] grid;
+    private float nodeDiameter { get { return nodeRadius * 2; } }
+    private int gridSizeX { get { return Mathf.RoundToInt(gameObject.GetComponent<Renderer>().bounds.size.x / nodeDiameter); } }
+    private int gridSizeY { get { return Mathf.RoundToInt(gameObject.GetComponent<Renderer>().bounds.size.z / nodeDiameter); } }
+
+    private void Start()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 origin = transform.position;//bottom left to create coordinate system
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+       Vector3 size = new Vector3(gridSizeX, 1, gridSizeY);
+       Gizmos.DrawWireCube(transform.position, size); 
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    private Vector3 worldPos;
+    private bool walkable; //avoid obstacles
+    
+
+    public Node(Vector3 worldPos, bool walkable)
+    {
+        this.worldPos = worldPos;
+        this.walkable = walkable;
+    }
+}
